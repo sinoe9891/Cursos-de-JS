653348feeb526ed220a159935c45de4cc61a92f5
sinoe9891 <sinoeproducciones@gmail.com>
2020-09-29 00:49:32 -0600

    Variables y 01 app

--- JS2_Udemy/Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js/app.js
+++ JS2_Udemy/Curso+JS+Moderno/Curso JS Moderno/01-Introduccion/js/app.js
@@ -6,11 +6,52 @@ const nombre = prompt('Cual es tu nombre?');
 const apellido = prompt('Cual es tu apellido?');
 
 //Toma el nombre y lo muestra en pantalla
-document.querySelector('.contenido1').innerHTML =  nombre + ' está aprendiendo JS Moderno';
+document.querySelector('.contenido1').innerHTML = nombre + ' está aprendiendo JS Moderno';
 
-//Muestra en consola el nombre
+// Interpolación de texto
+var nombres = nombre + " " +  apellido;
+console.log(`¡Hola ${nombres}!`);
+
+
 console.log(nombre);
-console.log(nombre  + ' ' + apellido);
+//Muestra en consola el nombre en mayuscula y minuscula
+console.log(nombre.toUpperCase() + ' ' + apellido.toLowerCase());
+//Saber el primer carcater del nombre ingresado
+console.log(nombre.charAt(0));
+var str = nombre.charAt(1) + nombre.charAt(2);
+console.log(str);
+var str = nombre.substr(1, 2);
+console.log(str);
+
+//Saber cuantos caracteres tienen un string
+console.log(nombre.length);
+//Timpo de carga Inicio y Final
+console.time('Hola');
+console.timeEnd('Hola');
+//Consideraciones en JS
+console.log('Hola');
+//Se puede poner sin punto y coma
+//Funcion
+function hola() {
+	console.log('ok');
+}
+hola();
+
+//EsLint
+//npm i -g eslint
+//creamos el archivo .eslintrc.json
+// {
+// 	"parserOptions": {
+// 		"ecmaVersion": 6
+// 	},
+// 	"rules": {
+// 		"semi": ["error", "always"]
+// 	}
+// }
+//Ejecutamos
+//eslint js/app.js 
+//Forzamos solución con:
+//eslint js/app.js --fix
 
 
 //Comentar Código
---
+++ JS2_Udemy/Curso+JS+Moderno/Curso JS Moderno/02-Variables/js/01-app.js
@@ -0,0 +1,32 @@
+//Asignar e inicializar variable
+var producto = "monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar
+producto = 'Monitor de 19 pulgadas';
+console.log(producto);
+
+// JS es un lenguaje de tipo dinamico, no se especifica tipo de dato
+producto = 20;
+console.log(producto);
+
+// Se pueden inicializar sin valior y asignarlo después
+var disponible;
+disponible = false;
+
+disponible = true;
+
+// Inicializar multiples variables
+var categoria = 'computadoras',
+	marca = "Marca Famosa",
+	calificacion = 5;
+
+//Las variables no pueden iniciar con números
+
+
+//Camel Case (La mas usada)
+var nombreProducto;
+// UnderScore
+var nombre_producto_categoria;
+// Pascal Case
+var nombreProducto;
